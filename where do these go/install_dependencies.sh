db9b781729fde12cc148be9508867bd7eceabd54
Broncorniclops <antonmalmkar@gmail.com>
2025-06-09 23:26:01 -0500

    Delete where do these go directory

--- where do these go/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing Python dependencies from requirements.txt..."
-if [ -f requirements.txt ]; then
-    pip3 install -r requirements.txt
-else
-    echo "Error: requirements.txt not found in the current directory."
-    exit 1
-fi
